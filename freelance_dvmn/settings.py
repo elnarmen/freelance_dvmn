160d8032bf7a982957b881c81aed5aa1c87a0772
leeny_the_bear <leeny@inbox.ru>
2023-02-22 20:55:53 +0600

    add tg token to the settings

--- freelance_dvmn/settings.py
+++ freelance_dvmn/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env("SECRET_KEY")
-
+TG_TOKEN = env("TG_TOKEN")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DEBUG", False)
 
