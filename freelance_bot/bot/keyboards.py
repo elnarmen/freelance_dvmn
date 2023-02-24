95aee1123a80c2e7daf81afd9c4612edef723d88
PaSeRouS <paser96@mail.ru>
2023-02-24 23:22:52 +0300

    Добавление новых функция для клавиатур пользователей

--- freelance_bot/bot/keyboards.py
+++ freelance_bot/bot/keyboards.py
@@ -1,5 +1,7 @@
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 
+from freelance_bot.models import Customer
+
 
 def main_menu_keyboard():
     keyboard = [
@@ -9,10 +11,28 @@ def main_menu_keyboard():
     return InlineKeyboardMarkup(keyboard)
 
 
-def customer_menu_keyboard():
+def customer_menu_keyboard(customer: Customer):
+    if customer.tariff:
+        keyboard = [
+            [InlineKeyboardButton("Создать заказ", callback_data='create_order')],
+            [InlineKeyboardButton("Мои заказы", callback_data='customer_orders')],
+            [InlineKeyboardButton("К выбору роли", callback_data='back_to_main_menu')]
+        ]
+    else:
+        keyboard = [
+            [InlineKeyboardButton("Выбрать тариф", callback_data='choose_tariff')],
+        ]
+    
+    return InlineKeyboardMarkup(keyboard)
+
+
+def freelancer_menu_keyboard():
     keyboard = [
-        [InlineKeyboardButton("Выбрать тариф", callback_data='choose_tariff')],
+        [InlineKeyboardButton("Выбрать заказ", callback_data='choose_order')],
+        [InlineKeyboardButton("Мои заказы", callback_data='freelancer_orders')],
+        [InlineKeyboardButton("К выбору роли", callback_data='back_to_main_menu')]
     ]
+    
     return InlineKeyboardMarkup(keyboard)
 
 
@@ -20,7 +40,13 @@ def subscribe_keyboard():
     keyboard = [
         [InlineKeyboardButton("Эконом", callback_data='economy_tariff')],
         [InlineKeyboardButton("Стандарт", callback_data='standart_tariff')],
-        [InlineKeyboardButton("VIP", callback_data='vip_tariff')],
-        []
+        [InlineKeyboardButton("VIP", callback_data='vip_tariff')]
+    ]
+    return InlineKeyboardMarkup(keyboard)
+
+
+def back_to_main_menu_keyboard():
+    keyboard = [
+        [InlineKeyboardButton("К выбору роли", callback_data='back_to_main_menu')]
     ]
     return InlineKeyboardMarkup(keyboard)
