ec9ad0dbb0f6b4b3b8ceb7ee76a3a2f9f7dc12e1
PaSeRouS <paser96@mail.ru>
2023-02-28 15:27:43 +0300

    Сделал нередактируемым поле created_at модели Messsage

--- freelance_bot/admin.py
+++ freelance_bot/admin.py
@@ -3,7 +3,10 @@ from django.contrib import admin
 from .models import Customer, Message, Order, Tariff
 
 # Register your models here.
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    readonly_fields = ['created_at']
+
 admin.site.register(Customer)
-admin.site.register(Message)
 admin.site.register(Order)
 admin.site.register(Tariff)
