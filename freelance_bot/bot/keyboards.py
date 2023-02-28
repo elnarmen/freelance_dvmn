ab1acde9d9cae41b0ad15a09d7fe3c39c56fa76b
leeny_the_bear <leeny@inbox.ru>
2023-02-28 13:30:04 +0600

    add a chat button for a freelancer

--- freelance_bot/bot/keyboards.py
+++ freelance_bot/bot/keyboards.py
@@ -114,7 +114,8 @@ def available_order_keyboard():
 def freelancer_order_keyboard():
     keyboard = [
         [InlineKeyboardButton("Назад к списку заказов", callback_data="back")],
-        [InlineKeyboardButton("Отказаться от заказа", callback_data="cancel_order")]
+        [InlineKeyboardButton("Отказаться от заказа", callback_data="cancel_order")],
+        [InlineKeyboardButton("Чат", callback_data="chat")]
     ]
 
     return InlineKeyboardMarkup(keyboard)
